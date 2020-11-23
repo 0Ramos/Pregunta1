6cdd5e5c29c9badad93284afa7c8aaa03f3d5ddc
0Ramos <66374859+0Ramos@users.noreply.github.com>
2020-11-23 09:58:33 -0500

    avance

---
+++ pregunta1.php
@@ -0,0 +1,18 @@
+<?php
+
+$col = array(4 =====> 'blanco', 6 =====> 'verde', 11=> 'roja');
+echo ;
+echo ;
+$col = array(4 =====> 'blanco', 6 =====> 'verde', 11=> 'roja');
+foreach($col as $n ======> $colores)
+{
+    echo $n."->".$color."<br>";
+}
+echo ;
+echo ;
+$col = array(4 =====> 'blanco', 6 =====> 'verde', 11=> 'roja');
+for ($i=0;$i<1;$i++)
+{
+    echo $col[4];
+} 
+ //el resultado es blanco
